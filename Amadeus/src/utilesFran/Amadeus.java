c8e71caca27fd16bb490d41e3868533a2976827b
Fran <fralg100@gmail.com>
2018-12-14 08:33:34 +0100

    Amadeus

---
+++ Amadeus/src/utilesFran/Amadeus.java
@@ -0,0 +1,422 @@
+package utilesFran;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * Clase de utilidades (control de variables, aplicación de try catch, ...)
+ * de uso personal
+ * @author Francisco Antonio Lorente Girol
+ * @version 1.0
+ * */
+
+public class Amadeus {
+
+	BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+	boolean error = false;
+	Random random=new Random();
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Integer
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número entero controlado
+	 * */
+	public int controlaInt() throws IOException {
+		int entero = 0;
+		do {
+			try {
+				entero = Integer.parseInt(entrada.readLine());
+				error = false;
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido");
+				error = true;
+			}
+		} while (error);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Integer
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número entero controlado positivo (incluyendo el 0)
+	 * */
+	public int controlaIntPositivo() throws IOException {
+		int entero = 0;
+		do {
+			try {
+				entero = Integer.parseInt(entrada.readLine());
+				error = false;
+				if (entero < 0)
+					System.out.println("Introduzca un valor positivo");
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido");
+				error = true;
+			}
+		} while (error || entero < 0);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Integer
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número entero controlado entre 0 y 100
+	 * */
+	public int controlaIntPorcentaje() throws IOException {
+		int entero = 0;
+		do {
+			try {
+				entero = Integer.parseInt(entrada.readLine());
+				error = false;
+				if (entero < 0)
+					System.out.println("Introduzca un valor mayor a 0");
+				if (entero > 100)
+					System.out.println("Introduzca un valor menor a 100");
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido");
+				error = true;
+			}
+		} while (error || entero < 0 || entero > 100);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Double
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número del tipo double controlado
+	 * */
+	public double controlaDouble() throws IOException {
+		double entero = 0;
+		do {
+			try {
+				entero = Double.parseDouble(entrada.readLine());
+				error = false;
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido, vuelva a introducirlo");
+				error = true;
+			}
+		} while (error);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Double
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número del tipo double positivo (incluyendo 0)
+	 * */
+	public double controlaDoublePositivo() throws IOException {
+		double entero = 0;
+		do {
+			try {
+				entero = Double.parseDouble(entrada.readLine());
+				error = false;
+				if (entero < 0)
+					System.out.println("Introduzca un valor positivo");
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido, vuelva a introducirlo");
+				error = true;
+			}
+		} while (error || entero < 0);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Double
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número del tipo double controlado entre 0 y 100
+	 * */
+	public double controlaDoublePorcentaje() throws IOException {
+		double entero = 0;
+		do {
+			try {
+				entero = Double.parseDouble(entrada.readLine());
+				error = false;
+				if (entero < 0)
+					System.out.println("Introduzca un valor mayor a 0");
+				if (entero > 100)
+					System.out.println("Introduzca un valor menor a 100");
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido");
+				error = true;
+			}
+		} while (error || entero < 0 || entero > 100);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Float
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número del tipo float controlado
+	 * */
+	public float controlaFloat() throws IOException {
+		float entero = 0;
+		do {
+			try {
+				entero = Float.parseFloat(entrada.readLine());
+				error = false;
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido, vuelva a introducirlo");
+				error = true;
+			}
+		} while (error);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de un dato tipo Float
+	 * y aplica los try catch necesarios durante el proceso.
+	 * @return Número del tipo float positivo (incluye el 0)
+	 * */
+	public float controlaFloatPositivo() throws IOException {
+		float entero = 0;
+		do {
+			try {
+				entero = Float.parseFloat(entrada.readLine());
+				error = false;
+				if (entero < 0)
+					System.out.println("Introduzca un valor positivo");
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("Valor no válido, vuelva a introducirlo");
+				error = true;
+			}
+		} while (error || entero < 0);
+		return entero;
+	}
+
+	/**
+	 * Método que aplica solicita la introducción de una cadena de caracteres por teclado.
+	 * El método comprueba si se ha escrito 's', 'sí', 'si', 'yes','y', 'no','n'
+	 * @return <ul>
+	 * <li>true Si se ha escrito 's', 'sí', 'si', 'yes','y'
+	 * <li>false Si se ha escrito 'n', 'no'
+	 * </ul>
+	 * */
+	public boolean afirmaODesmiente() throws IOException {
+		String respuesta;
+		do {
+			respuesta = entrada.readLine();
+			error = false;
+			if (respuesta.equalsIgnoreCase("si") || respuesta.equalsIgnoreCase("y") || respuesta.equalsIgnoreCase("sí")
+					|| respuesta.equalsIgnoreCase("s") || respuesta.equalsIgnoreCase("yes"))
+				return true;
+			else if (respuesta.equalsIgnoreCase("no") || respuesta.equalsIgnoreCase("n"))
+				return false;
+			else {
+				error = true;
+				System.out.println("Valor no válido, introduzca si o no");
+			}
+		} while (error);
+		return false;
+	}
+
+	/**
+	 * Solicita por consola la entrada por teclado de una cadena de caracteres.
+	 * Comprueba que dicha cadena no esté vacía o que no sean todos espacios en blanco.
+	 * @return Cadena de caracteres válida
+	 * */
+	public String recibeTexto() throws IOException {
+		String respuesta;
+		boolean vacio = true;
+		char c;
+		do {
+			respuesta = entrada.readLine();
+			for (int i = 0; i < respuesta.length(); i++) {
+				c = respuesta.charAt(i);
+				if (!Character.isWhitespace(c)) {
+					vacio = false;
+					break;
+				}
+			}
+			if (vacio)
+				System.out.println("El texto no debe estar vacío, introduzca datos");
+		} while (vacio);
+		return respuesta;
+	}
+	
+	/**
+	 * Comprueba que una cadena no esté vacía o que no sean todos espacios en blanco.
+	 * @param s Cadena de caracteres a comprobar
+	 * @return Cadena de caracteres válida
+	 * */
+	public String compruebaTexto(String s) throws IOException {
+		String respuesta=s;
+		boolean vacio = true;
+		char c;
+		do {
+			for (int i = 0; i < respuesta.length(); i++) {
+				c = respuesta.charAt(i);
+				if (!Character.isWhitespace(c)) {
+					vacio = false;
+					break;
+				}
+			}
+			if (vacio) {
+				System.out.println("El texto no debe estar vacío, introduzca datos");
+				respuesta = entrada.readLine();
+			}
+		} while (vacio);
+		return respuesta;
+	}
+	
+	/**
+	 * Método que comprueba que el DNI tenga el formato adecuado de 8 dígitos y una letra al final
+	 * @param s DNI a comprobar
+	 * @return DNI validado
+	 * */
+	public String compruebaNIF(String s) throws IOException {
+		String respuesta=s;
+		boolean error = false;
+		char c;
+		
+		do {
+			error=false;
+			while(respuesta.length()!=9) {
+				System.out.println("Longitud del DNI incorrecta, vuelva a introducirlo");
+				respuesta=recibeTexto();
+			}
+			for (int i = 0; i < 8; i++) {
+				c = respuesta.charAt(i);
+				if (!Character.isDigit(c)) {
+					error = true;
+					break;
+				}
+			}
+			if(!Character.isAlphabetic(respuesta.charAt(8)))
+				error=true;
+			if (error) {
+				System.out.println("DNI no válido. Vuélvalo a introducir");
+				respuesta = entrada.readLine();
+			}
+		} while (error);
+		return respuesta;
+	}
+	
+	/*/////////////////////////////////////////////////////////////////////*/
+	/*/ARRAYS//////////////////////////////////////////////////////////////*/
+	/*/////////////////////////////////////////////////////////////////////*/
+	
+	public int[] rellenaArray(int[] array, int maxVal) {
+		int val;
+		for (int i = 0; i < array.length; i++) {
+			val=random.nextInt(maxVal)+1;
+			array[i]=val;
+		}
+		return array;
+		
+	}
+	
+	public int[] creaArrayLleno(int tamanho,int maxVal) {
+		int[] array=new int[tamanho];
+		return rellenaArray(array,maxVal);
+		
+	}
+	
+	public void imprimeArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+	}
+	
+	public int[] sumaArray(int[]a, int[]b) {
+		int capacidad=a.length;
+		int[] suma=new int[capacidad];
+		for (int i = 0; i < suma.length; i++) {
+			suma[i]=a[i]+b[i];
+		}
+		return suma;
+	}
+	
+	/*/////////////////////////////////////////////////////////////////////*/
+	/*/MATRICES////////////////////////////////////////////////////////////*/
+	/*/////////////////////////////////////////////////////////////////////*/
+	
+	public int[][] rellenaMatriz(int[][] matriz, int maxVal) {
+		int val;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				val=random.nextInt(maxVal)+1;
+				matriz[i][j]=val;
+			}
+		}
+		return matriz;	
+	}
+	
+	public int[][] creaMatriz(int filas, int columnas, int maxVal) {
+		int[][]matriz=new int[filas][columnas];
+		return rellenaMatriz(matriz, maxVal);
+	}
+	
+	public int[][] sumaMatriz(int[][]a, int[][]b) {
+		int nFilas=a.length;
+		int nColumnas=a[0].length;
+		int[][] suma=new int[nFilas][nColumnas];
+		for (int i = 0; i < suma.length; i++) {
+			for (int j = 0; j < suma[i].length; j++) {
+				suma[i][j]=a[i][j]+b[i][j];
+			}
+		}
+		return suma;
+	}
+	
+	public void imprimeMatriz(int[][]matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j]+" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public int mayorEnLaMatriz(int[][] matriz) {
+		int aux=matriz[0][0];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j]>aux)
+					aux=matriz[i][j];
+			}
+		}
+		return aux;
+	}
+	
+	public void posicionesMayorEnLaMatriz(int[][] matriz) {
+		int mayor=mayorEnLaMatriz(matriz);
+		System.out.println("El mayor número se encuentra en la/s posición/es: ");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(mayor==matriz[i][j])
+					System.out.print("["+i+"]["+j+"]  ");
+			}
+		}
+	}
+	
+	public int menorEnLaMatriz(int[][] matriz) {
+		int aux=matriz[0][0];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j]<aux)
+					aux=matriz[i][j];
+			}
+		}
+		return aux;
+	}
+	
+	public void posicionesMenorEnLaMatriz(int[][] matriz) {
+		int mayor=menorEnLaMatriz(matriz);
+		System.out.println("El menor número se encuentra en la/s posición/es: ");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(mayor==matriz[i][j])
+					System.out.print("["+i+"]["+j+"]  ");
+			}
+		}
+	}
+}
